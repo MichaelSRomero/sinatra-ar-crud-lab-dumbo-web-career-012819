fc462e792c748436a130ca412834fc978d7ef52b
MichaelRomero <liikemike@aim.com>
2019-02-21 00:15:06 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,48 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  # Directs to a form
+  get '/articles/new' do
+    erb :new
   end
+
+  # Gets called when user clicks "SUBMIT" from #get(/articles/new)
+  post '/articles' do
+    @article = Article.create(params)
+    redirect "/articles/#{@article.id}"
+  end
+
+  # Displays all
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  # Displays specific article
+  get '/articles/:id' do
+    get_article
+    erb :show
+  end
+
+  # Displays form with object attributes filled in
+  get '/articles/:id/edit' do
+    get_article
+    erb :edit
+  end
+
+  # Updates existing article with new input
+  patch '/articles/:id' do
+    get_article.update(title: params[:title], content: params[:content])
+    redirect "articles/#{params[:id]}"
+  end
+
+  delete '/articles/:id' do
+    get_article.delete
+    redirect "/articles"
+  end
+
+  def get_article
+    @article = Article.find(params[:id])
+  end
+
 end
